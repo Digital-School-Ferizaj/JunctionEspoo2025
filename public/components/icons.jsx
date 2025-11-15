f7bca1e07d22f0cc85b0c70b1b13e3b8732ef335
lirakH <lirakhamiti@hotmail.com>
2025-11-16 00:33:19 +0100

    icons fixed

--- public/components/icons.jsx
+++ public/components/icons.jsx
@@ -1,57 +1,34 @@
-const lucideIcons = window.LucideReact || {};
-const {
-    Shield,
-    Heart,
-    Calendar,
-    BookOpen,
-    Users,
-    Star,
-    Activity,
-    Droplet,
-    PlusCircle,
-    Phone,
-    Menu,
-    Send,
-    Save,
-    AlertTriangle,
-    Sun,
-    Moon,
-    X,
-    MessageCircle,
-    UserCheck,
-    Leaf,
-    Bookmark,
-} = lucideIcons;
+const ICON_BASE_CLASS = 'material-symbols-rounded text-[1.35rem] leading-none';
 
-const ICON_PROPS = { size: 24, strokeWidth: 1.8 };
-const createIcon = (Component, extraProps = {}) => {
-    if (!Component) {
-        return () => null;
-    }
-    return (props = {}) => <Component {...ICON_PROPS} {...extraProps} {...props} />;
+const createIcon = (iconName) => {
+    return ({ className = '', style } = {}) => (
+        <span className={`${ICON_BASE_CLASS} ${className}`} style={style}>
+            {iconName}
+        </span>
+    );
 };
 
-const ShieldIcon = createIcon(Shield);
-const HeartIcon = createIcon(Heart);
-const CalendarIcon = createIcon(Calendar);
-const BookOpenIcon = createIcon(BookOpen);
-const UsersIcon = createIcon(Users);
-const SparklesIcon = createIcon(Star);
-const ActivityIcon = createIcon(Activity);
-const DropletsIcon = createIcon(Droplet);
-const PillIcon = createIcon(PlusCircle);
-const PhoneIcon = createIcon(Phone, { size: 20 });
-const MenuIcon = createIcon(Menu);
-const SendIcon = createIcon(Send, { size: 20 });
-const SaveIcon = createIcon(Save, { size: 20 });
-const AlertTriangleIcon = createIcon(AlertTriangle);
-const SunIcon = createIcon(Sun);
-const MoonIcon = createIcon(Moon);
-const XIcon = createIcon(X);
-const ChatBubbleIcon = createIcon(MessageCircle);
-const BuddyIcon = createIcon(UserCheck);
-const WellnessLeafIcon = createIcon(Leaf);
-const MemorySparkIcon = createIcon(Bookmark);
+const ShieldIcon = createIcon('shield');
+const HeartIcon = createIcon('favorite');
+const CalendarIcon = createIcon('calendar_month');
+const BookOpenIcon = createIcon('menu_book');
+const UsersIcon = createIcon('groups');
+const SparklesIcon = createIcon('auto_awesome');
+const ActivityIcon = createIcon('monitor_heart');
+const DropletsIcon = createIcon('water_drop');
+const PillIcon = createIcon('medication');
+const PhoneIcon = createIcon('call');
+const MenuIcon = createIcon('menu');
+const SendIcon = createIcon('send');
+const SaveIcon = createIcon('save');
+const AlertTriangleIcon = createIcon('warning');
+const SunIcon = createIcon('sunny');
+const MoonIcon = createIcon('dark_mode');
+const XIcon = createIcon('close');
+const ChatBubbleIcon = createIcon('chat');
+const BuddyIcon = createIcon('diversity_3');
+const WellnessLeafIcon = createIcon('spa');
+const MemorySparkIcon = createIcon('bookmark');
 
 window.AmilyIcons = {
     ShieldIcon,
